3d031e93eeab4e147293de256f15a10e6e82cf7b
erlanggadwi <erlanggdwip@gmail.com>
2021-09-14 12:10:55 +0700

    commit pertama

---
+++ bot/js/click.js
@@ -0,0 +1,3 @@
+function openLink() {
+    window.open('https://api.whatsapp.com/send?phone=6282131434105&text=/menu', '_blank')
+}
--- js/click.js
+++
@@ -1,3 +0,0 @@
-function openLink() {
-    window.open('https://api.whatsapp.com/send?phone=994404753665&text=!help', '_blank')
-}
